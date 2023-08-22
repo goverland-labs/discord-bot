a792397d4e17f423a038a59ec6e2d651ddd77638
Gelato-B2B <105652074+Gelato-B2B@users.noreply.github.com>
2023-08-22 16:57:32 +0400

    Update govbot.py

--- govbot.py
+++ govbot.py
@@ -1,21 +1,37 @@
 import asyncio
 import discord
+from discord.ext import commands
 import requests
 import os
 import pandas as pd
 import urllib.parse
+import sqlite3
 from dotenv import load_dotenv
-# import time
 
 load_dotenv()
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
-
 govid = os.getenv("GOVID")
 discordtoken = os.getenv("DISCORD_TOKEN")
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+listening = False
+
+
+@bot.command()
+async def start(ctx):
+    global listening  # Use the global flag
+    listening = True
+    await ctx.send("Goverland bot activated. Now listening for proposals.")
+
+
+@bot.command()
+async def stop(ctx):
+    global listening  # Use the global flag
+    listening = False
+    await ctx.send("Goverland bot deactivated. No longer listening for proposals.")
 
 
 def get_data_with_session(session_id):
@@ -62,44 +78,43 @@ async def send_message_to_discord(channel, title, link):
 
 
 async def listen_to_url(session_id):
-    last_data = None
 
     while True:
-        try:
-            rawdata = get_data_with_session(session_id)
-            data = pd.DataFrame(rawdata)
-            title = data["proposal"][0]["title"]
-            link = data["proposal"][0]["link"]
-            formatted_url = urllib.parse.quote(link, safe=':/#')
-            feed_id = data['id'][0]
+        if listening:
+            try:
+                rawdata = get_data_with_session(session_id)
+                data = pd.DataFrame(rawdata)
+                title = data["proposal"][0]["title"]
+                link = data["proposal"][0]["link"]
+                formatted_url = urllib.parse.quote(link, safe=':/#')
+                feed_id = data['id'][0]
 
-            # print(f"New data: {title}")
+                # print(f"New data: {title}")
 
-            # Send the new event message to Discord chat
-            await send_message_to_discord(channel, title, formatted_url)
+                # Send the new event message to Discord chat
+                await send_message_to_discord(channel, title, formatted_url)
 
-            mark_item_as_read(feed_id)
+                mark_item_as_read(feed_id)
 
-        except Exception as e:
-            print(f"Error occurred: {e}")
+            except Exception as e:
+                print(f"Error occurred: {e}")
 
-        # Set the interval (in seconds) to wait before checking for updates
+            # Set the interval (in seconds) to wait before checking for updates
         await asyncio.sleep(10)
 
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    print(f'We have logged in as {bot.user}')
 
     # Get the channel where the bot is connected (assuming it is only connected to one channel)
     global channel
-    channel = client.get_channel(client.guilds[0].text_channels[0].id)
+    channel = bot.get_channel(bot.guilds[0].text_channels[0].id)
 
     # Start the listening task
-    client.loop.create_task(listen_to_url(govid))
+    bot.loop.create_task(listen_to_url(govid))
 
 if __name__ == "__main__":
     # Replace this with your actual Discord bot token
     token = discordtoken
-
-    client.run(token)
+    bot.run(token)
