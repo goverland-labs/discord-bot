ef53d0239243067db5487bb82ca428e71459b9a8
Gelato-B2B <105652074+Gelato-B2B@users.noreply.github.com>
2023-08-23 14:53:09 +0400

    Update govbot.py

--- govbot.py
+++ govbot.py
@@ -6,6 +6,7 @@ import os
 import pandas as pd
 import urllib.parse
 import sqlite3
+from datetime import datetime, timedelta
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -19,19 +20,65 @@ discordtoken = os.getenv("DISCORD_TOKEN")
 bot = commands.Bot(command_prefix='!', intents=intents)
 listening = False
 
-
-@bot.command()
-async def start(ctx):
-    global listening  # Use the global flag
-    listening = True
-    await ctx.send("Goverland bot activated. Now listening for proposals.")
+con = sqlite3.connect("subs.db")
+cur = con.cursor()
 
 
 @bot.command()
-async def stop(ctx):
+async def gov_sub(ctx):
+    server_id = ctx.guild.id
+    print(server_id)
+
+    query = "SELECT serverid FROM subs WHERE serverid = ?"
+    res = cur.execute(query, (server_id,))
+    check = res.fetchone() is True
+
+    if check:
+        await ctx.send("You are already subscribed")
+    else:
+        value = server_id
+        result_string = f"{value}"
+        url = "https://inbox.goverland.xyz/auth/guest"
+        data = {
+            "device_id": result_string
+        }
+        response = requests.post(url, json=data)
+        response.raise_for_status()  # Check for any HTTP errors
+
+        data = response.json()  # Assuming the response is in JSON format
+        session_id = data["session_id"]
+
+        query = "INSERT INTO subs (serverid, sessionid) VALUES (?, ?)"
+        cur.execute(query, (server_id, session_id))
+        con.commit()
+        await ctx.send("Successfully subscribed to Goverland bot")
+
+
+@bot.command()
+async def gov_start(ctx):
+    server_id = ctx.guild.id
+
+    query = "SELECT serverid FROM subs WHERE serverid = ?"
+    res = cur.execute(query, (server_id,))
+    check = res.fetchone() is True
+
+    if check:
+        await ctx.send("You are already subscribed")
+    else:
+        global listening  # Use the global flag
+        listening = True
+        await ctx.send("Goverland bot activated. Now listening for proposals.")
+
+
+@bot.command()
+async def gov_stop(ctx):
     global listening  # Use the global flag
     listening = False
     await ctx.send("Goverland bot deactivated. No longer listening for proposals.")
+
+
+def subscribe():
+    url = "https://inbox.goverland.xyz/auth/guest"
 
 
 def get_data_with_session(session_id):
@@ -73,6 +120,7 @@ def mark_item_as_read(feed_id):
 
 
 async def send_message_to_discord(channel, title, link):
+    print(channel)
     message = f"New proposal: [{title}]({link})"
     await channel.send(message)
 
@@ -84,17 +132,28 @@ async def listen_to_url(session_id):
             try:
                 rawdata = get_data_with_session(session_id)
                 data = pd.DataFrame(rawdata)
-                title = data["proposal"][0]["title"]
-                link = data["proposal"][0]["link"]
-                formatted_url = urllib.parse.quote(link, safe=':/#')
-                feed_id = data['id'][0]
 
-                # print(f"New data: {title}")
+                provided_datetime = data["created_at"]
+                provided_datetime_obj = datetime.strptime(
+                    provided_datetime, '%Y-%m-%dT%H:%M:%SZ')
+                current_datetime = datetime.utcnow()
+                one_day_ago = current_datetime - timedelta(days=1)
 
-                # Send the new event message to Discord chat
-                await send_message_to_discord(channel, title, formatted_url)
+                if provided_datetime_obj > one_day_ago:
+                    title = data["proposal"][0]["title"]
+                    link = data["proposal"][0]["link"]
+                    formatted_url = urllib.parse.quote(link, safe=':/#')
+                    feed_id = data['id'][0]
 
-                mark_item_as_read(feed_id)
+                    # print(f"New data: {title}")
+
+                    # Send the new event message to Discord chat
+                    await send_message_to_discord(channel, title, formatted_url)
+
+                    mark_item_as_read(feed_id)
+                else:
+                    print(
+                        "The provided datetime is not higher than current timestamp minus one day.")
 
             except Exception as e:
                 print(f"Error occurred: {e}")
@@ -107,12 +166,23 @@ async def listen_to_url(session_id):
 async def on_ready():
     print(f'We have logged in as {bot.user}')
 
-    # Get the channel where the bot is connected (assuming it is only connected to one channel)
-    global channel
-    channel = bot.get_channel(bot.guilds[0].text_channels[0].id)
+    for guild in bot.guilds:
+        server_id = guild.id
 
-    # Start the listening task
-    bot.loop.create_task(listen_to_url(govid))
+        # Get the channel where the bot is connected (assuming it is only connected to one channel)
+        global channel
+        channel = bot.get_channel(bot.guilds[0].text_channels[0].id)
+
+        server_id = guild.id
+        query = "SELECT sessionid FROM subs WHERE serverid = ?"
+        res = cur.execute(query, (server_id,))
+        data = res.fetchone()
+
+        if data:
+            session_id = data[0]
+            for text_channel in guild.text_channels:
+                # Start the listening task
+                bot.loop.create_task(listen_to_url(session_id, text_channel))
 
 if __name__ == "__main__":
     # Replace this with your actual Discord bot token
